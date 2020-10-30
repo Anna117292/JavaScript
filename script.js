efb98a657016720a33371a0175d096b43a5a9496
Lubarskaya <ovod125@bk.ru>
2020-10-30 15:40:10 +0300

    lesson1

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+//задание 1
+
+let tempCels=10;
+let tempFar=(9/5)*tempCels+32;
+alert(tempFar);
+
+
+
+
+//задание 2
+
+let name="Vasiliy"
+let admin="Vasiliy";
+alert(admin);
+
+
+/*   задание 3 
+выражение 1000+"108"  будет равно 1000108    */
+
+let result=1000+"108";
+alert(result);
